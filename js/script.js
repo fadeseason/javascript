be1d8e8191e8ba764f90ac3d0c8ce1bb82138ec0
fades <example@gmail.com>
2022-08-25 21:00:51 +0500

    add new homework in script.js

--- js/script.js
+++ js/script.js
@@ -10,8 +10,24 @@ const personalMovieDb = {
 
 for (let i = 0; i < 2; i++){
     const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = +prompt('Насколько оцените его?', '');
-    personalMovieDb.movies[a] = b;
+        b = +prompt('Насколько оцените его?', '');
+     if (a != null && a !='' && b != null && b !='' && a.length <50){
+        personalMovieDb.movies[a] = b;
+        console.log('done');  
+        } else {
+            console.log('error');
+            i--;
+        } 
+}
+
+if (personalMovieDb.count <10){
+    console.log('Просмотрено мало фильмов');
+} else if (personalMovieDb.count >= 10 && personalMovieDb.count <30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDb.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Error:(');
 }
 
 console.log(personalMovieDb);
