d03a75367fe5c947c574def2fb39ebc9f459fccd
fades <fadeseason@yandex.ru>
2022-09-03 12:13:55 +0500

    forEach massive

--- js/main.js
+++ js/main.js
@@ -63,3 +63,26 @@
 // }
 
 // Массивы и псевдомассивы
+
+const arr = [1, 2, 3, 6, 8];
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри массива ${arr} `);
+});
+
+//arr.pop(); // удаляет значение с конца массива
+
+// arr.push(10); // добавляет (в данном случае 10) в конец массива элемент
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+for (let value of arr) {
+    console.log(value);
+}
